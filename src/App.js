5994289435dd9e4bdc835b9d094133610eb6e630
Diksha220 <60913047+Diksha220@users.noreply.github.com>
2020-09-03 16:43:57 +0530

    finished styling header

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import HomePage from './pages/homepage/homepage.component';
 import {Route, Switch} from 'react-router-dom';
 import './App.css';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 
 function App() {
   return (
     <>
+      <Header/>
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route path='/shop/hats' component={ShopPage} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
     </>
   );
