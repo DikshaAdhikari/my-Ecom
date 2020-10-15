2d7e0dadbb501f139fbe0ef6791be8a6e62e040a
Diksha220 <60913047+Diksha220@users.noreply.github.com>
2020-10-15 12:55:59 +0530

    implemented redux

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -13,38 +13,38 @@ const config = {
     measurementId: "G-MG4BXEX4NM"
 };
 
-export const createUserProfileDocument = async (userAuth, additionalData) => {
-    if(!userAuth) return;
-    const userRef = firestore.doc(`users/${userAuth.uid}`);
-    const snapshot =await userRef.get();
-
-    if(!snapshot.exists){
-        const{displayName, email} = userAuth;
-        const createdAt = new Date();
-
-        try{
-            await userRef.set({
-                displayName,
-                email,
-                createdAt,
-                ...additionalData
-            })
-        } catch(e){
-            console.log('error creating user', e.message);
-        }
-    }
-
-    return userRef;
-};
 firebase.initializeApp(config);
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      });
+    } catch (error) {
+      console.log('error creating user', error.message);
+    }
+  }
+
+  return userRef;
+};
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
 const provider = new firebase.auth.GoogleAuthProvider();
-
-// always trigger the google popup whenever we use GoogleAuthProvider for authentication and signIn
-provider.setCustomParameters({prompt: 'select_account'}); 
-
+provider.setCustomParameters({ prompt: 'select_account' });
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
 export default firebase;
