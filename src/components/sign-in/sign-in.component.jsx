8551afa0995f488251eb29a3fc66bbaf3c25b758
Diksha220 <60913047+Diksha220@users.noreply.github.com>
2020-12-16 19:35:16 +0530

    basics done

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,73 +1,74 @@
 import React from 'react';
+
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
-import {auth, signInWithGoogle} from '../../firebase/firebase.utils';
+
+import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
+
 import './sign-in.styles.scss';
 
-class SignIn extends React.Component{
-    constructor(props){
-        super(props);
+class SignIn extends React.Component {
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            email : '',
-            password : ''
-        }
-    }
-
-    handleSubmit = async e => {
-        e.preventDefault();
-        const {email, password} =this.state;
-
-        try{
-            await auth.signInWithEmailAndPassword(email, password);
-            this.setState({ email: '', password:'' });
-        }
-        catch(e){
-            console.error(e)
-        }
+    this.state = {
+      email: '',
+      password: ''
     };
+  }
 
-    handleChange = e => {
-        const {value, name} = e.target;
+  handleSubmit = async event => {
+    event.preventDefault();
 
-        this.setState({ [name]: value })
-    };
+    const { email, password } = this.state;
 
-    render(){
-        return(
-            <div className='sign-in'>
-                <h2 className='title'>I already have an account</h2>
-                <span>Sign in with your email and password</span>
-
-                <form onSubmit={this.handleSubmit}>
-                    <FormInput 
-                        name='email' 
-                        value={this.state.email} 
-                        required 
-                        type='email' 
-                        handleChange={this.handleChange}
-                        label='Email'
-                    />
-                    <FormInput 
-                        name='password' 
-                        value={this.state.password} 
-                        required 
-                        type='password' 
-                        handleChange={this.handleChange}
-                        label='Password'
-                    />
-                    
-                    <div className='buttons'>
-                    <CustomButton type='submit'>Sign In</CustomButton>
-                    <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
-                        Sign In With Google
-                    </CustomButton>
-                    </div>
-                    
-                </form>
-            </div>
-        );
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      this.setState({ email: '', password: '' });
+    } catch (error) {
+      console.log(error);
     }
+  };
+
+  handleChange = event => {
+    const { value, name } = event.target;
+
+    this.setState({ [name]: value });
+  };
+
+  render() {
+    return (
+      <div className='sign-in'>
+        <h2>I already have an account</h2>
+        <span>Sign in with your email and password</span>
+
+        <form onSubmit={this.handleSubmit}>
+          <FormInput
+            name='email'
+            type='email'
+            handleChange={this.handleChange}
+            value={this.state.email}
+            label='email'
+            required
+          />
+          <FormInput
+            name='password'
+            type='password'
+            value={this.state.password}
+            handleChange={this.handleChange}
+            label='password'
+            required
+          />
+          <div className='buttons'>
+            <CustomButton type='submit'> Sign in </CustomButton>
+            <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+              Sign in with Google
+            </CustomButton>
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default SignIn;
---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = 'pk_test_51HyszVHSjAPI675BgOzj3k9tHjkx19mTEW2yb4RJB2bY0vw2pj8zTHuUGoWakDzpnwyS7Qu5Q64Dqgy9Yqcmrj3I00m3qelcfE';
+
+  const onToken = token => {
+    console.log(token);
+    alert('Payment Succesful!');
+  };
+
+  return (
+    <StripeCheckout
+      label='Pay Now'
+      name='OLIVIA'
+      billingAddress
+      shippingAddress
+      image='https://svgshare.com/i/CUz.svg'
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel='Pay Now'
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
