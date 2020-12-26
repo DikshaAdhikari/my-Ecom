0655855431e28be03d96cc527af90fcfc9a2b543
Diksha220 <60913047+Diksha220@users.noreply.github.com>
2020-12-26 13:53:59 +0530

    mobile friendly

--- src/App.js
+++ src/App.js
@@ -3,12 +3,11 @@ import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
-import './App.css';
-
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import CheckoutPage from './pages/checkout/checkout.component';
+import { GlobalStyle } from './global.styles';
 
 import Header from './components/header/header.component';
 
@@ -46,6 +45,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
+        <GlobalStyle />
         <Header />
         <Switch>
           <Route exact path='/' component={HomePage} />
